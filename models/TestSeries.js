16f0f642d5013b2da707b98bc11cb50f8c911cac
abhishekprajgs11-jpg <abhishekprajgs1.1@gmail.com>
2026-08-11 14:41:08 +0530

    Add models/TestSeries.js

---
+++ models/TestSeries.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const testSeriesSchema = new mongoose.Schema({
+  year: {
+    type: String,
+    required: true,
+  },
+  coaching: {
+    type: String,
+    required: true,
+  },
+  testCode: {
+    type: String,
+    required: true,
+  },
+  questionPdfId: {
+    type: String,
+    required: true,
+  },
+  solutionPdfId: {
+    type: String,
+    required: false, // In case solution is added later
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('TestSeries', testSeriesSchema);
